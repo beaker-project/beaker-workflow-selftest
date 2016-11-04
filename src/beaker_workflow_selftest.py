feaedc7bd9695e9f99f0d863b2100099a8f555aa
Dan Callaghan <dcallagh@redhat.com>
2016-11-04 10:17:55 +1000

    drop Fedora 22, add Fedora 24

--- src/beaker_workflow_selftest.py
+++ src/beaker_workflow_selftest.py
@@ -43,16 +43,16 @@ def distros_variants_arches(multihost=False):
             ('Workstation', ['x86_64']),
             ('ComputeNode', ['x86_64']),
         ]),
-        ('Fedora-22', [
-            ('Server', ['i386', 'x86_64']),
-            ('Workstation', ['i386', 'x86_64']),
-            ('Cloud', ['i386', 'x86_64']),
-        ]),
         ('Fedora-23', [
             # s390x omitted until initrd.img > 32 MB issue is solved (see bz1322235)
             ('Server', ['i386', 'x86_64', 'ppc64', 'ppc64le', 'aarch64']),
             ('Workstation', ['i386', 'x86_64']),
             ('Cloud', ['i386', 'x86_64', 'ppc64', 'ppc64le', 'aarch64']),
+        ]),
+        ('Fedora 24', [
+            # s390x omitted until initrd.img > 32 MB issue is solved (see bz1322235)
+            ('Server', ['i386', 'x86_64', 'ppc64', 'ppc64le', 'aarch64']),
+            ('Workstation', ['i386', 'x86_64']),
         ]),
     ]
     for distro, variants_arches in supported_combinations:
